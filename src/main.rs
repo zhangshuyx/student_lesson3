20102a8f41766bbc65b7c2c9c438d671ecd61433
zhangshuyx <zhangshuyx@gmail.com>
2023-07-20 15:56:38 +0800

    add vector and consume

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+// 声明枚举类型
 #[derive(Debug)]
 enum Sex {
     Male,
@@ -28,6 +29,7 @@ enum Course {
     History,
 }
 
+// 声明结构体
 #[derive(Debug)]
 struct Student {
     name: String,
@@ -38,12 +40,14 @@ struct Student {
     course: Course,
 }
 
+// 声明结构体的方法
 impl Student {
     fn print_info(&self) {
         println!("Info of {} is:\nAge: {}\nSex: {:?}\nShetuan: {:?}\nClass: {:?}\nCourse: {:?}\n", self.name, self.age, self.sex, self.shetuan, self.class, self.course)
     }
 }
 
+// 主函数，实例化结构体，调用方法，改变结构体内容
 fn main() {
     let mut xiaoming: Student = Student{
         name : String::from("Xiaoming"),
@@ -66,9 +70,15 @@ fn main() {
     xiaoming.print_info();
     xiaohong.print_info();
 
+    // 修改结构体的项
     xiaoming.shetuan = SheTuan::Kaifang;
     xiaoming.age = 22;
     xiaoming.print_info();
 
-    
+    // 构建vector，消费vector
+    let students = vec![xiaoming, xiaohong];
+    for item in students {
+        item.print_info()
+    }
+
 }
